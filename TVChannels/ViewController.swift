a31bac808d8b04b8f940c076e57c620aa9610adb
iuada110 <mykyta.lyzohubov@raiffeisen.ua>
2023-06-09 19:11:46 +0300

    init project

---
+++ TVChannels/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TVChannels
+//
+//  Created by iuada110 on 09.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
+
